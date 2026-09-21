9e5a1b039b6c6bd770dcbea15f58dbf2075a99a1
yoannrc94-creator <yoann.rc94@gmail.com>
2026-09-21 03:37:15 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,41 @@
-  plugins {
+plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-}      
-    
-    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+}
+
+android {
+    namespace = "com.jarvis.assistant"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.jarvis.assistant"
+        minSdk = 26
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+}
+
+configurations.all {
+    resolutionStrategy {
+        force("org.jetbrains.kotlin:kotlin-stdlib:1.9.22")
+        force("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.9.22")
+        force("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.9.22")
+    }
 }
